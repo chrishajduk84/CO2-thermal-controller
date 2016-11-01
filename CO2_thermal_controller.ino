3fbe4947649d716d1b90d84fc8a9bb9fbce0101a
Chris Hajduk <chrishajduk84@hotmail.com>
2016-11-01 18:43:22 -0400

    Added bang bang controller and other minor changes

--- CO2_thermal_controller.ino
+++ CO2_thermal_controller.ino
@@ -22,6 +22,7 @@ const uint8_t CHIP_SELECT_PIN = 53; // Using standard CS line (SS)
 // SCK & MISO are defined by Arduiino
 const uint8_t VCC = 48; // Powering board straight from Arduino Pro Mini
 const uint8_t GND = 49;
+const uint8_t RELAY = 46;
 
 //Instantiate the class
 SparkFunMAX31855k probe(CHIP_SELECT_PIN, VCC, GND);
@@ -29,10 +30,14 @@ SparkFunMAX31855k probe(CHIP_SELECT_PIN, VCC, GND);
 //Control Variables
 float setpointTemp = 0;
 float currentTemp = 0;
+bool bbState = false;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  pinMode(RELAY, OUTPUT);
+  
+  //Ask user for input temperature
   Serial.println("To begin heating the CO2 tank please enter the starting temperature (C): ");
   int response = 0;
   while (response == 0){
@@ -51,8 +56,14 @@ void setup() {
   }
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Check Control Inputs
+  int response = Serial.readString().toInt();
+  if (response) setpointTemp = response;
+  currentTemp = probe.readTempC();
+
+  //Output information
   char buff1[4];
   char buff2[4];
   dtostrf(setpointTemp,3,1,buff1);
@@ -61,7 +72,18 @@ void loop() {
   String textTemp2 = String(buff2);
   Serial.println("Setpoint Temperature: " + textTemp1 + "C, Current Temperature: "  + textTemp2 + "C" );
 
-  float currentTemp = probe.readTempC();
+  
   delay(750);
+}
+
+//Tolerance is how much the controller is allowed to over shoot when heating/cooling
+bool bbController(float setpoint, float current, float tolerance){
+  if (current < setpoint - tolerance){
+    bbState = true;
+  }
+  else if (current > setpoint + tolerance){
+    bbState = false;
+  }
+  return bbState;
 }
 
